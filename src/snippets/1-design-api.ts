78014e528043c8d2d9868a1e19611db66c05eee2
Adrian Połubiński <polubis@wp.pl>
2022-06-23 10:55:22 +0200

    Update comments

--- src/snippets/1-design-api.ts
+++ src/snippets/1-design-api.ts
@@ -1,5 +1,5 @@
 const ExampleComponent = () => {
-  // As first argument we passing initial values
+  // As the first argument, we passing initial values
   // As second argument validation config
   const form = useForm(
     { username: "", password: "" },
@@ -7,26 +7,26 @@ const ExampleComponent = () => {
   );
 
   form.keys; // Returns ['username', 'password']
-  form.values; // Object which contains values
+  form.values; // An Object which contains values
 
   form.invalid; // Determines is form invalid
   form.valid; // Negation of invalid
   form.progress; // Progress in percentage
   form.validCount; // Count of valid fields
   form.invalidCount; // Count of invalid fields
-  form.errors; // Object which contains errors or empty strings
+  form.errors; // An Object which contains errors or empty strings
 
-  form.touched; // Indicates is any value in form changed
+  form.touched; // Indicates if any value in the form changed
   form.untouched; // Negation of touched
-  form.confirmed; // Indicates that form was confirmed
+  form.confirmed; // Indicates that the form was confirmed
   form.unconfirmed; // Negation of confirmed
 
-  form.set("username", "pablo"); // Sets value and runs validation
+  form.set("username", "pablo"); // Sets value/runs validation
   form.change({ target: { value: "pablo", name: "username" } }); // Same as above but handled via native change event
 
   form.reset(); // Resets state to initial
 
-  form.confirm(); // Runs validation for whole form
+  form.confirm(); // Runs validation for the whole form
   form.submit({ preventDefault: () => {} }); // Same as above but in addition calls preventDefault()
 
   const sub = form.on("username").subscribe(({ key, value }) => {
