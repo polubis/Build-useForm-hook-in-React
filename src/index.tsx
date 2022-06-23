428662e5b7f18df42b190da25771c44e514c586c
Adrian Połubiński <polubis@wp.pl>
2022-06-23 09:28:14 +0200

    Update visualization

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import WebFont from "webfontloader";
 
-import "styles/index.scss";
+import "./styles/index.scss";
 
 WebFont.load({
   google: {
-    families: ["sans-serif", "Montserrat:300,400,500,700&display=swap"],
-  },
+    families: ["sans-serif", "Montserrat:300,400,500,700&display=swap"]
+  }
 });
 
 const root = ReactDOM.createRoot(
